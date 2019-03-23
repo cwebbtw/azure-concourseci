cdfd35825106ed238759bc64f9b936c79f758b3d
Christopher <cwebb@christophersmbp.lan>
2019-03-23 16:08:16 +0000

    Generating keys for concourse

--- terraform/cloud-init/config.sh
+++ terraform/cloud-init/config.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-AZ_REPO=$(lsb_release -cs)
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $(lsb_release -cs) main" | \
     sudo tee /etc/apt/sources.list.d/azure-cli.list
 
 curl -L https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 
-sudo apt-get install apt-transport-https
-sudo apt-get update && sudo apt-get install -y azure-cli libssl-dev libffi-dev python-dev python-pip
-sudo pip install ansible[azure]
+# Install Azure CLI and ansible together due to known compatibility issues
+apt-get update
+apt-get apt-transport-https install -y azure-cli libssl-dev libffi-dev python-dev python-pip
+pip install ansible[azure]
 
 cd ~
 
@@ -16,6 +16,6 @@ git clone https://github.com/cwebbtw/azure-concourseci.git
 
 cd azure-concourseci/ansible
 
-ansible-playbook --connection=local --inventory localhost, concourse.yml --tags "full"
+ansible-playbook --connection=local --inventory localhost, playbook.yml --tags all
 
 exit 0
